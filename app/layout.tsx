7430f6316f62b34813c5fd3be82c73b7a1e8d056
Christian Sturm <c.sturm@avm.de>
2025-01-03 22:19:33 +0100

    feat: add Inter font to by default to all elements

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${inter.variable} ${geistMono.variable} antialiased`}
+                className={`${inter.variable} ${geistMono.variable} antialiased font-sans`}
             >
                 {children}
             </body>
