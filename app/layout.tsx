734562edd5d3bf824aadbd8f53f9e12c06543bcf
Christian Sturm <c.sturm@avm.de>
2025-01-03 18:20:24 +0100

    chore: format all

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,17 +18,17 @@ import "./globals.css";
 // };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+            // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
