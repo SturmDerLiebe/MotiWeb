f677d9ec344f621ec32648dd73748dbb99104c37
Christian Sturm <c.sturm@avm.de>
2025-01-04 22:20:03 +0100

    feat: extend Button with onClick

--- components/buttons/Primary.tsx
+++ components/buttons/Primary.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { type PropsWithChildren } from "react";
+import type { MouseEventHandler, PropsWithChildren } from "react";
 
 export function PrimaryButton({
     className = "",
+    onClick,
     children,
-}: PropsWithChildren<{ className?: string }>) {
+}: PropsWithChildren<{
+    onClick: MouseEventHandler<HTMLButtonElement>;
+    className?: string;
+}>) {
     return (
         <button
             className={`bg-grey-7 text-white py-3 rounded-full font-semibold ring-offset-4 focus-visible:ring-8  hover:ring-8 active:bg-blue-2 active:ring-0 disabled:bg-grey-6 ${className}`}
-            onClick={() => {}}
+            onClick={onClick}
         >
             {children}
         </button>
