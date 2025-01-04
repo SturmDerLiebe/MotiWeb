5bd5cde81cb2947a8f6b0aa27e8cb7afbfd28d96
Christian Sturm <c.sturm@avm.de>
2025-01-05 00:03:59 +0100

    feat: add secondary button

---
+++ components/buttons/Secondary.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import type { MouseEventHandler, PropsWithChildren } from "react";
+import { BaseButton } from "./Base";
+
+export function SecondaryButton({
+    className = "",
+    onClick,
+    children,
+}: PropsWithChildren<{
+    onClick: MouseEventHandler<HTMLButtonElement>;
+    className?: string;
+}>) {
+    return (
+        <BaseButton
+            className={`bg-white border-2 border-grey-7 text-grey-7 ring-grey-2
+active:bg-grey-2 active:border-grey-5
+disabled:bg-white disabled:text-grey-1 disabled:border-grey-1 ${className}`}
+            onClick={onClick}
+        >
+            {children}
+        </BaseButton>
+    );
+}
