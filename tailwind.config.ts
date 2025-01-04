4b649065cd9b9a52438183397044fd7d30a6f7f2
Christian Sturm <c.sturm@avm.de>
2025-01-04 01:52:02 +0100

    feat: adjust 3xl font size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,9 @@ export default {
                 sans: ["var(--font-inter)"],
                 mono: ["var(--font-geist-mono)"],
             },
+            fontSize: {
+                "3xl": "1.75rem",
+            },
             backgroundImage: {
                 MotiMateLogo: "url('/illustrations/vectors/MotiMateLogo.svg')",
             },
