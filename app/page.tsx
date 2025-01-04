eb4e68d9d5561b287684490f8a58b1208c3f2c4b
Christian Sturm <c.sturm@avm.de>
2025-01-04 20:07:30 +0100

    feat: customize index

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,43 @@
-import Image from "next/image";
+import { ExplainerCard } from "@/components/cards/Explainer";
+import HandsUsingApp from "@/public/illustrations/bitmaps/HandsUsingApp.png";
+import StretchingPerson from "@/public/illustrations/vectors/StretchingPerson.svg";
+import TriumphCumPeople from "@/public/illustrations/vectors/TriumphCupPeople.svg";
+import JumpingRopePerson from "@/public/illustrations/vectors/JumpingRopePerson.svg";
+import { PrimaryButton } from "@/components/buttons/Primary";
 
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={180}
-                    height={38}
-                    priority
+        <main className="flex flex-col mx-4">
+            <section className="flex gap-4 mt-1">
+                <ExplainerCard
+                    title="Create a fitness group"
+                    paragraph="Stay connected and motivated by creating  a group with your friends! Invite your workout buddies to join you on this fitness journey, and encourage each other to crush your goals"
+                    source={HandsUsingApp}
+                    altText="A set of two hands. The right one holding a phone with the group-chat of the MotiMate-App running. The left hand swiping down to read new messages."
                 />
-                <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                    <li className="mb-2">
-                        Get started by editing{" "}
-                        <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-                            app/page.tsx
-                        </code>
-                        .
-                    </li>
-                    <li>Save and see your changes instantly.</li>
-                </ol>
-
-                <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read our docs
-                    </a>
-                </div>
-            </main>
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/file.svg"
-                        alt="File icon"
-                        width={16}
-                        height={16}
-                    />
-                    Learn
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
-                </a>
-            </footer>
-        </div>
+                <ExplainerCard
+                    title="Create a fitness group"
+                    paragraph="Stay connected and motivated by creating  a group with your friends! Invite your workout buddies to join you on this fitness journey, and encourage each other to crush your goals"
+                    source={StretchingPerson}
+                    altText="A Person stretching down with their hands out straight."
+                />
+                <ExplainerCard
+                    title="Share Your Success"
+                    paragraph="Share your workout photos as a proof with friends in your group. Every time you send a picture, your progress bar is updated"
+                    source={TriumphCumPeople}
+                    altText="Two people reaching the goal of a marathon and running towards a winner cup"
+                />
+                <ExplainerCard
+                    title="Penalty is your motivation!"
+                    paragraph={`Send 10€ to a trusted member who manages the group fund when you don’t meet your fitness goal.
+Use it for a group gathering anytime!`}
+                    source={JumpingRopePerson}
+                    altText="A person jumping rope"
+                />
+            </section>
+            <PrimaryButton className="w-1/4 self-center mt-6">
+                Get Startet
+            </PrimaryButton>
+        </main>
     );
 }
