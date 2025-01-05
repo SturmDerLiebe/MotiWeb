29037939d65ea3c23a4e06320a5dbaa99bf6fdeb
Christian Sturm <c.sturm@avm.de>
2025-01-05 15:43:39 +0100

    refac: extract Heading5 styles

--- components/cards/Explainer.tsx
+++ components/cards/Explainer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import type { StaticImport } from "next/dist/shared/lib/get-img-props";
+import { HeadingStyles } from "@/styles/Headings";
 
 export function ExplainerCard({
     title,
@@ -16,7 +17,7 @@ export function ExplainerCard({
         <article className="flex-1 shadow rounded-xl border bg-white">
             <Image className="w-full shadow-inner" src={source} alt={altText} />
             <div className="px-2 pb-2">
-                <h2 className="text-3xl font-bold pt-8">{title}</h2>
+                <h2 className={`${HeadingStyles.Heading5} pt-8`}>{title}</h2>
                 <p className="text-base font-medium pt-2">{paragraph}</p>
             </div>
         </article>
---
+++ styles/Headings.ts
@@ -0,0 +1,1 @@
+export const HeadingStyles = { Heading5: "text-3xl font-bold" };
