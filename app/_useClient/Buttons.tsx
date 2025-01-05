ddcad41aafe7ce0b464bf6fbafcba26283d5e38a
Christian Sturm <c.sturm@avm.de>
2025-01-05 01:17:38 +0100

    feat: add onClick router action

--- app/_useClient/Buttons.tsx
+++ app/_useClient/Buttons.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import { PrimaryButton } from "@/components/buttons/Primary";
+import { useRouter } from "next/navigation";
 
 export function GetStartedButton() {
-    "use client";
+    const router = useRouter();
     return (
-        <PrimaryButton className="w-1/4 self-center" onClick={() => {}}>
+        <PrimaryButton
+            className="w-1/4 self-center"
+            onClick={() => {
+                router.push("/authenticate");
+            }}
+        >
             Get Startet
         </PrimaryButton>
     );
