709697b40c7dc15c6c7307203ced87cfd89d2a98
Christian Sturm <c.sturm@avm.de>
2025-01-06 22:42:40 +0100

    feat: use new PasswrodInputs

--- app/authenticate/register/_useClient/Inputs.tsx
+++ app/authenticate/register/_useClient/Inputs.tsx
@@ -5,6 +5,7 @@ import { BaseInputComponent } from "@/components/inputs/Base";
 import { EmailInputComponent } from "@/components/inputs/Email";
 import { PasswordInputComponent } from "@/components/inputs/Password";
 import { UsernameRegex } from "@/constants/regex/validation";
+import { useState } from "react";
 
 export function RegistrationForm() {
     return (
@@ -26,21 +27,48 @@ export function RegistrationForm() {
                 placeholder="Enter your E-Mail"
             />
 
+            <PasswordFields />
+
+            <PrimaryButton>Create Account</PrimaryButton>
+        </form>
+    );
+}
+
+function PasswordFields() {
+    const PLACEHOLDER = "••••••••";
+    const MIN_LENGTH = 8;
+
+    const [chosenPassword, setChosenPassword] = useState("");
+
+    return (
+        <>
             <PasswordInputComponent
                 labelText="Password"
                 assistiveText="Needs to be at least 8 characters long"
-                placeholder="••••••••"
-                minLength={8}
+                placeholder={PLACEHOLDER}
+                minLength={MIN_LENGTH}
+                savePassword={(chosenPassword) => {
+                    setChosenPassword(chosenPassword);
+                }}
             />
 
             <PasswordInputComponent
                 labelText="Confirm password"
                 assistiveText="Test if you chose the Password you wanted"
-                placeholder="••••••••"
-                minLength={8}
+                placeholder={PLACEHOLDER}
+                minLength={MIN_LENGTH}
+                onBlur={(
+                    { target: { value: repeatedPassword } },
+                    { current: input },
+                ) => {
+                    input?.setCustomValidity(
+                        repeatedPassword === chosenPassword
+                            ? ""
+                            : "The Passwords do not match!",
+                    );
+                    input?.reportValidity();
+                }}
             />
-
-            <PrimaryButton>Create Account</PrimaryButton>
-        </form>
+        </>
     );
 }
