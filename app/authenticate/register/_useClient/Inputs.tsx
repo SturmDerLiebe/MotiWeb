d22cf1c76bbfeaed92243a50c988b5e28490c5ed
Christian Sturm <c.sturm@avm.de>
2025-01-06 19:39:52 +0100

    feat: use new input components

--- app/authenticate/register/_useClient/Inputs.tsx
+++ app/authenticate/register/_useClient/Inputs.tsx
@@ -2,6 +2,8 @@
 
 import { PrimaryButton } from "@/components/buttons/Primary";
 import { BaseInputComponent } from "@/components/inputs/Base";
+import { EmailInputComponent } from "@/components/inputs/Email";
+import { PasswordInputComponent } from "@/components/inputs/Password";
 import { UsernameRegex } from "@/constants/regex/validation";
 
 export function RegistrationForm() {
@@ -19,26 +21,25 @@ export function RegistrationForm() {
                 pattern={UsernameRegex.source}
             />
 
-            <BaseInputComponent
-                labelText="Username (Nickname)"
-                assistiveText=" Only Letters e.g. Jane Doe"
-                placeholder="How should we call you?"
-                pattern={UsernameRegex.source}
+            <EmailInputComponent
+                labelText="E-Mail"
+                placeholder="Enter your E-Mail"
             />
 
-            <BaseInputComponent
-                labelText="Username (Nickname)"
-                assistiveText="e.g. Jane"
-                placeholder="How should we call you?"
-                pattern={UsernameRegex.source}
+            <PasswordInputComponent
+                labelText="Password"
+                assistiveText="Needs to be at least 8 characters long"
+                placeholder="••••••••"
+                minLength={8}
             />
 
-            <BaseInputComponent
-                labelText="Username (Nickname)"
-                assistiveText="e.g. Jane"
-                placeholder="How should we call you?"
-                pattern={UsernameRegex.source}
+            <PasswordInputComponent
+                labelText="Confirm password"
+                assistiveText="Test if you chose the Password you wanted"
+                placeholder="••••••••"
+                minLength={8}
             />
+
             <PrimaryButton>Create Account</PrimaryButton>
         </form>
     );
