a19a54e8aa17d35731297fe25e348b1646596513
Christian Sturm <c.sturm@avm.de>
2025-01-06 22:43:23 +0100

    feat: add div around input for further extensions

--- components/inputs/Base.tsx
+++ components/inputs/Base.tsx
@@ -12,13 +12,14 @@ export function BaseInputComponent({
     pattern,
     minLength,
     required,
+    onBlur,
 }: BaseInputComponentProps): React.JSX.Element {
     return (
         <label
             htmlFor={labelText}
             className={`paragraph9 text-grey-5 flex items-center gap-2`}
         >
-            {labelText}
+            <span className="flex-1">{labelText}</span>
 
             <BaseInputField
                 placeholder={placeholder}
@@ -27,10 +28,13 @@ export function BaseInputComponent({
                 pattern={pattern}
                 minLength={minLength}
                 id={labelText}
+                onBlur={onBlur}
             />
 
-            {type === "email" ? null : (
-                <p className="paragraph8 text-grey-6 [:user-invalid+&]:text-error-2 before:content-['🛈'] [:user-invalid+&]:before:text-error-2 before:text-xl flex items-center gap-2">
+            {type === "email" ? (
+                <div className="flex-1" />
+            ) : (
+                <p className="flex-1 paragraph8 text-grey-6 [:user-invalid+&]:text-error-2 before:content-['🛈'] [:user-invalid+&]:before:text-error-2 before:text-xl flex items-center gap-2">
                     {assistiveText}
                 </p>
             )}
@@ -45,20 +49,25 @@ function BaseInputField({
     minLength,
     required = true,
     id,
+    onBlur,
 }: BaseInputFieldProps) {
     const InputRef = useRef<HTMLInputElement | null>(null);
 
     return (
-        <input
-            placeholder={placeholder}
-            type={type}
-            required={required}
-            pattern={pattern}
-            minLength={minLength}
-            className={`flex-1 paragraph4 py-3 px-4 text-grey-7 border rounded-lg shadow-sm [&:user-invalid]:border-error-2 placeholder:text-grey-4 placeholder-shown:border-grey-4 focus:border-grey-5 focus:outline-none`}
-            id={id}
-            ref={InputRef}
-            onBlur={() => {}}
-        />
+        <div className="flex-1 border rounded-lg shadow-sm flex [&:has(:user-invalid)]:border-error-2 [&:has(:placeholder-shown)]:border-grey-4 [&:has(:focus)]:border-grey-5">
+            <input
+                placeholder={placeholder}
+                type={type}
+                required={required}
+                pattern={pattern}
+                minLength={minLength}
+                className={`flex-1 rounded-lg paragraph4 py-3 px-4 text-grey-7 placeholder:text-grey-4 focus:outline-none`}
+                id={id}
+                ref={InputRef}
+                onBlur={(event) => {
+                    onBlur?.(event, InputRef);
+                }}
+            />
+        </div>
     );
 }
