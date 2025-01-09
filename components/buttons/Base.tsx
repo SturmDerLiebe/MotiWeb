a45ec38d9d15c97e9a9533dff30ba37b6b27bd2d
Christian Sturm <c.sturm@avm.de>
2025-01-09 17:57:57 +0100

    feat: add disabled to Button Compoents

--- components/buttons/Base.tsx
+++ components/buttons/Base.tsx
@@ -3,12 +3,14 @@
 import { BaseButtonProps } from "./type";
 
 export function BaseButton({
+    disabled = false,
     className = "",
     onClick = () => {},
     children,
 }: BaseButtonProps) {
     return (
         <button
+            disabled={disabled}
             className={`py-3 rounded-full text-lg font-semibold ring-offset-4 ring-offset-white focus-visible:ring-8 hover:ring-8 active:ring-0 active:ring-offset-0 ${className}`}
             onClick={onClick}
         >
--- components/buttons/Primary.tsx
+++ components/buttons/Primary.tsx
@@ -4,12 +4,14 @@ import { BaseButton } from "./Base";
 import { BaseButtonProps } from "./type";
 
 export function PrimaryButton({
+    disabled,
     className = "",
     onClick,
     children,
 }: BaseButtonProps) {
     return (
         <BaseButton
+            disabled={disabled}
             className={`bg-grey-7 text-white ring-blue-2 active:bg-blue-2 disabled:bg-grey-6 ${className}`}
             onClick={onClick}
         >
