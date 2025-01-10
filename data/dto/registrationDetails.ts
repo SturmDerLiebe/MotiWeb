bca74ef989682a5fe93425020c9f358193b7f351
Christian Sturm <c.sturm@avm.de>
2025-01-10 14:48:30 +0100

    feat: add build from object static method

--- data/dto/registrationDetails.ts
+++ data/dto/registrationDetails.ts
@@ -2,6 +2,14 @@
  * A Data Transfer Object containing details about a new user to be registered
  */
 export class RegistrationDetails {
+    static buildFromObject({
+        username = "",
+        email = "",
+        password = "",
+    }: Record<string, string>) {
+        return new RegistrationDetails(username, email, password);
+    }
+
     constructor(
         public username: string,
         public email: string,
