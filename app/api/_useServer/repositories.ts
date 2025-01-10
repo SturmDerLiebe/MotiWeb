ba79e150ead779a7d99530ff1f7e578bb8f05be6
Christian Sturm <c.sturm@avm.de>
2025-01-10 14:55:26 +0100

    feat: add session repo to singletons

--- app/api/_useServer/repositories.ts
+++ app/api/_useServer/repositories.ts
@@ -1,21 +1,10 @@
-import { SessionRepository } from "@/data/interface/sessionRepository";
-import { BaseRepository } from "@/data/repository/base";
 import { UserRepository } from "@/data/repository/user";
+import { UseServerSessionRepository } from "./repository/session";
 
-class UseServerSessionRepository implements SessionRepository {
-    sessionId = "";
-
-    async saveSessionId(sessionId: string) {
-        this.sessionId = sessionId;
-    }
-
-    async hasSessionId() {
-        return this.sessionId.trim().length > 0;
-    }
-}
+export const SessionRepositoryInstance = new UseServerSessionRepository();
 
 export const UserRepositoryInstance = new UserRepository({
     apiBaseUrl: UserRepository.Api.mockaroo,
     publicApiKey: process.env.NEXT_PUBLIC_MOCKAROO_KEY ?? "",
-    sessionRepository: new UseServerSessionRepository(),
+    sessionRepository: SessionRepositoryInstance,
 });
