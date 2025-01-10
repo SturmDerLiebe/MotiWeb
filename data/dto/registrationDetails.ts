bce3e509366ca26bfecd9feafd246991326bcb72
Christian Sturm <c.sturm@avm.de>
2025-01-10 18:17:38 +0100

    feat: add form field names to dto

--- data/dto/registrationDetails.ts
+++ data/dto/registrationDetails.ts
@@ -2,6 +2,12 @@
  * A Data Transfer Object containing details about a new user to be registered
  */
 export class RegistrationDetails {
+    static formFieldNames = {
+        username: "username",
+        email: "email",
+        password: "password",
+    };
+
     static buildFromObject({
         username = "",
         email = "",
