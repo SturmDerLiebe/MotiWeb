ee6832ab5e04fcb0f292de97fd363d6032b74135
Christian Sturm <c.sturm@avm.de>
2025-01-10 14:49:29 +0100

    fix: revert SessionRepo Interface

--- data/interface/sessionRepository.ts
+++ data/interface/sessionRepository.ts
@@ -1,8 +1,8 @@
 export interface SessionRepository {
-    readonly sessionId: string;
+    readSessionId(): Promise<string>;
 
     saveSessionId(sessionId: string): Promise<void>;
 
     /** Retrieves and Checks if {@link sessionId} is not empty */
-    hasSessionId(): Promise<boolean>;
+    hasValidSessionId(): Promise<boolean>;
 }
